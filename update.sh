7b36a78f44a08ec10ab62f6241d22df14f31947f
Martijn Braam <martijn@brixit.nl>
2020-06-25 14:42:57 +0200

    Added update script

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget http://download.geonames.org/export/dump/cities1000.zip -O cities1000.zip
+wget http://download.geonames.org/export/dump/admin1CodesASCII.txt -O admin1CodesASCII.txt
+wget http://download.geonames.org/export/dump/admin2Codes.txt -O admin2Codes.txt
